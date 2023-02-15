7c422c9e27c5220db7000ee8636bc52398964a7c
Abdullah Ishtiaq <89216016+AbdullahIshtiaqq@users.noreply.github.com>
2023-02-16 00:40:43 +0500

    Add files via upload

---
+++ Homeworks/HW1/Exceptions.py
@@ -0,0 +1,14 @@
+class UserNotFound(Exception):
+    pass
+
+class InvalidChoice(Exception):
+    pass
+
+class InvalidSemester(Exception):
+    pass
+
+class InvalidCGPA(Exception):
+    pass
+
+class UserAlreadyExists(Exception):
+    pass
